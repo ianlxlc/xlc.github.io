d25ac3252e42a6961e003122ff3acc37db2db4fc
ianlxlc <77240540+ianlxlc@users.noreply.github.com>
2021-01-10 08:39:01 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function addToList() {
+		let userInput=document.getElementById("input").value; 
+		if (userInput === "") {
+			alert("Please provide the valid input");
+			return;
+		}
+		let ul=document.getElementById("list"); 
+		let li=document.createElement("li"); 
+		li.innerHTML=userInput; 
+		ul.appendChild(li); 
+
+		document.getElementById("input").value = "";
+		
+		let listItems = document.getElementsByTagName('li');
+		for (let i = 2; i < listItems.length; i += 3) {
+			listItems[i].style.color = 'red';
+		}
+	} 
+	
+	
